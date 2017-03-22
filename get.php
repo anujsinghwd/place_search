ff039a82ef91bd98d4d30d6308cc5ff8d0ff4297
anujsinghwd <anujsingh179@yahoo.com>
2017-03-23 00:32:36 +0530

    first commit

---
+++ get.php
@@ -0,0 +1,30 @@
+<?php 
+$place1 = $_POST['plac'];
+$place = str_replace(" ", "+", $place1);
+$html = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/json?query='.$place.'&key=API_KEY_PASTE_HERE');
+
+$json = json_decode($html, true);
+
+//echo '<pre>'.print_r($json, true).'</pre>';
+
+$address = $json['results'];
+$add = count($address);
+
+for($i=0;$i<$add;$i++){
+	$d = $json['results'][$i]['formatted_address'];
+	$nm = $json['results'][$i]['name'];
+	$la = $json['results'][$i]['geometry']['location']['lat'];
+	$ln = $json['results'][$i]['geometry']['location']['lng'];
+
+	$adres = array('name'=>$nm ,'address' => $d, 'latitude' => $la, 'longitude' => $ln);
+	$we[] = $adres;
+	//$lat[] = array('lat' => $la);
+	//$lng[] = array('lng' => $ln);
+
+    $result=json_encode($we);
+    //$latitude = json_encode($lat);
+    //$longitude = json_encode($lng);
+}
+
+echo $result;
+?>
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hey Stranger !</title>
+	<link rel="stylesheet" href="./css/style.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script>
+		
+		function get_data(){
+
+			var plac = document.getElementById('address').value;
+			//console.log(plac);
+                $.ajax({
+                  type : "POST",
+                  url : "get.php",
+                  data : {plac : plac},
+                  success : function(response){
+                    var dat = JSON.parse(response);
+                    var panel = $("#panel");
+                    for(var i=0;i<dat.length;i++){
+                        panel.append('<div class="panel panel-primary"><div class="panel-heading">'+ dat[i]["name"] +'</div>'+'<div class="panel-body">Address : '+ dat[i]["address"] + '</div>' + '<div class="panel-body">Latitude : '+ dat[i]["latitude"] +'<div>' + '<div class="panel-body">Longitude : ' + dat[i]["longitude"] + '</div></div>')
+                    }
+                  }
+                 }); 
+
+
+	}
+	
+	</script>
+</head>
+
+<body>
+
+<div class="container">
+<div class="col-sm-2"></div>
+<div class="col-sm-8">
+  <div class="form-inline">
+    <div class="form-group">
+      <label for="email" style="color: white;">Enter Place:</label>
+      <input type="text" class="form-control" size="70" id="address" placeholder="Enter place eg.. oyo delhi.." required/>
+    </div>
+    <button type="submit" onclick="get_data()" class="btn btn-default">Submit</button>
+  </div>
+
+  <div></div>
+
+
+  <div class="panel-group" style="margin-top: 10px;" id="panel">
+
+  </div>
+
+
+ </div>
+<div class="col-sm-2"></div>  
+</div>
+
+</body>
+</html>
